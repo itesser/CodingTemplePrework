a72dacd6be9248bba1a377f673762a95779e8faa
itesser <itesser@gmail.com>
2023-08-30 13:26:17 -0700

    R Exercises

---
+++ prework_exercises.R
@@ -0,0 +1,32 @@
+# Exercise 1
+x <- 39
+y <- 12
+z <- 5
+
+x + z
+
+x - y
+
+z * x
+
+y / z
+
+x %/% z
+
+y %% z
+
+# Exercise 2
+first_name <- "Sid"
+last_name <- "Hawker"
+full_name <- paste(first_name, last_name)
+
+# Exercise 3
+vector1 <- c(5, 8, 25, 46, 99)
+vector2 <- c(12, 20, 8, 30, 23)
+multi_vect <- vector1 * vector2
+
+# Exercise 4
+multi_vector[2:5]
+
+# Exercise 5
+my_first_sequence <- seq(21, 28, by=.25)
